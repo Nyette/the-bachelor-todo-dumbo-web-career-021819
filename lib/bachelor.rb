20fc1723a5bb67cb46f0d2c52b54d23689860a3b
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-07 06:51:36 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,50 @@
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = ""
+  data[season].each do |hash|
+    if hash["status"] == "Winner"
+      name = hash["name"]
+      name_array = name.split
+      # name_array = ["Tessa", "Horst"]
+      first_name += name_array[0]
+    end  
+  end
+  first_name
 end
 
+
 def get_contestant_name(data, occupation)
-  # code here
+  # returns "Beth Smalls" when passed data and "Nanny/Freelance Journalist"
+  name = ""
+  data.each do |season, array|
+    array.each do |hash|
+      if hash["occupation"] == occupation
+        name += hash["name"]
+      end
+    end
+  end
+  name
 end
+
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  counter = 0
+  data.each do |season, array|
+    array.each do |hash|
+      if hash["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
+
 def get_occupation(data, hometown)
-  # code here
+  # returns the occupation of the first contestant who hails from that hometown
+  
 end
 
 def get_average_age_for_season(data, season)
